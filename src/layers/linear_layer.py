bd3a6785d17af21a0094b3127562e876f2bf8600
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-01-07 10:56:27 +0300

    Create linear_layer.py

---
+++ src/layers/linear_layer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class LinearLayer(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+        self.linear = nn.Linear(in_features, out_features, bias=bias)
+
+    def forward(self, x):
+        return self.linear(x)
